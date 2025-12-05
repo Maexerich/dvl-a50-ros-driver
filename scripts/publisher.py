c6fbad9a586d86a16f5be4db180b28140953c425
Max Greiner <max.greiner.ch@gmail.com>
2025-12-05 14:02:10 +1100

    Created new message type where dvl dead-reckoning estimates are published to. Not yet tested.

--- scripts/publisher.py
+++ scripts/publisher.py
@@ -6,6 +6,7 @@ from time import sleep
 from std_msgs.msg import String
 from dvl_a50_ros_driver.msg import DVL
 from dvl_a50_ros_driver.msg import DVLBeam
+from dvl_a50_ros_driver.msg import DVLEstimate
 import select
 
 def connect():
@@ -14,6 +15,11 @@ def connect():
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		s.connect((TCP_IP, TCP_PORT))
 		s.settimeout(1)
+
+		# Reset dead-reckoning estimate on connection
+		reset_cmd = '{"command":"reset_position_local"}'
+		s.sendall(reset_cmd.encode("utf-8"))
+		rospy.loginfo("Connected to DVL at and sent reset_dead_reckoning command.")
 	except socket.error as err:
 		rospy.logerr("No route to host, DVL might be booting? {}".format(err))
 		sleep(1)
@@ -26,6 +32,7 @@ beam0 = DVLBeam()
 beam1 = DVLBeam()
 beam2 = DVLBeam()
 beam3 = DVLBeam()
+estimate = DVLEstimate()
 
 def getData():
 	global oldJson, s
@@ -54,6 +61,7 @@ def getData():
 def publisher():
 	pub_raw = rospy.Publisher('dvl/json_data', String, queue_size=10)
 	pub = rospy.Publisher('dvl/data', DVL, queue_size=10)
+	pub_estimate = rospy.Publisher('/dvl/estimate', DVLEstimate, queue_size=10)
 
 	rate = rospy.Rate(10) # 10hz
 	while not rospy.is_shutdown():
@@ -68,56 +76,77 @@ def publisher():
 		if do_log_raw_data:
 			rospy.loginfo(raw_data)
 			pub_raw.publish(raw_data)
-			if data["type"] != "velocity":
-				continue
+
+		# Get message type
+		msg_type = data.get("type", "unknown")
+
+		if msg_type == "velocity":
+			theDVL.header.stamp = rospy.Time.now()
+			theDVL.header.frame_id = "dvl_link"
+			theDVL.time = data["time"]
+			theDVL.velocity.x = data["vx"]
+			theDVL.velocity.y = data["vy"]
+			theDVL.velocity.z = data["vz"]
+			theDVL.fom = data["fom"]
+			theDVL.altitude = data["altitude"]
+			theDVL.velocity_valid = data["velocity_valid"]
+			theDVL.status = data["status"]
+			theDVL.form = data["format"]
+
+			beam0.id = data["transducers"][0]["id"]
+			beam0.velocity = data["transducers"][0]["velocity"]
+			beam0.distance = data["transducers"][0]["distance"]
+			beam0.rssi = data["transducers"][0]["rssi"]
+			beam0.nsd = data["transducers"][0]["nsd"]
+			beam0.valid = data["transducers"][0]["beam_valid"]
+
+			beam1.id = data["transducers"][1]["id"]
+			beam1.velocity = data["transducers"][1]["velocity"]
+			beam1.distance = data["transducers"][1]["distance"]
+			beam1.rssi = data["transducers"][1]["rssi"]
+			beam1.nsd = data["transducers"][1]["nsd"]
+			beam1.valid = data["transducers"][1]["beam_valid"]
+
+			beam2.id = data["transducers"][2]["id"]
+			beam2.velocity = data["transducers"][2]["velocity"]
+			beam2.distance = data["transducers"][2]["distance"]
+			beam2.rssi = data["transducers"][2]["rssi"]
+			beam2.nsd = data["transducers"][2]["nsd"]
+			beam2.valid = data["transducers"][2]["beam_valid"]
+
+			beam3.id = data["transducers"][3]["id"]
+			beam3.velocity = data["transducers"][3]["velocity"]
+			beam3.distance = data["transducers"][3]["distance"]
+			beam3.rssi = data["transducers"][3]["rssi"]
+			beam3.nsd = data["transducers"][3]["nsd"]
+			beam3.valid = data["transducers"][3]["beam_valid"]
+
+			theDVL.beams = [beam0, beam1, beam2, beam3]
+
+			pub.publish(theDVL)
+		
+		elif msg_type == "position_local":
+			estimate.header.stamp = rospy.Time.now()
+			estimate.header.frame_id = "odom"
+			estimate.ts = data["ts"]
+			estimate.x = data["x"]
+			estimate.y = data["y"]
+			estimate.z = data["z"]
+			estimate.std = data["std"]
+			estimate.roll = data["roll"]
+			estimate.pitch = data["pitch"]
+			estimate.yaw = data["yaw"]
+			estimate.status = data["status"]
+
+			pub_estimate.publish(estimate)
+			# print(f"Dead-reckoning euclidean distance {(estimate.x**2 + estimate.y**2 + estimate.z**2)**0.5:.2f} m")
+
+		elif msg_type == "response":
+			# This line is prints to terminal and publishes to /rosout
+			rospy.loginfo("DVL Response: {}".format(data.get("message", "")))
+
 		else:
-			if data["type"] != "velocity":
-				continue
-			pub_raw.publish(raw_data)
-
-		theDVL.header.stamp = rospy.Time.now()
-		theDVL.header.frame_id = "dvl_link"
-		theDVL.time = data["time"]
-		theDVL.velocity.x = data["vx"]
-		theDVL.velocity.y = data["vy"]
-		theDVL.velocity.z = data["vz"]
-		theDVL.fom = data["fom"]
-		theDVL.altitude = data["altitude"]
-		theDVL.velocity_valid = data["velocity_valid"]
-		theDVL.status = data["status"]
-		theDVL.form = data["format"]
-
-		beam0.id = data["transducers"][0]["id"]
-		beam0.velocity = data["transducers"][0]["velocity"]
-		beam0.distance = data["transducers"][0]["distance"]
-		beam0.rssi = data["transducers"][0]["rssi"]
-		beam0.nsd = data["transducers"][0]["nsd"]
-		beam0.valid = data["transducers"][0]["beam_valid"]
-
-		beam1.id = data["transducers"][1]["id"]
-		beam1.velocity = data["transducers"][1]["velocity"]
-		beam1.distance = data["transducers"][1]["distance"]
-		beam1.rssi = data["transducers"][1]["rssi"]
-		beam1.nsd = data["transducers"][1]["nsd"]
-		beam1.valid = data["transducers"][1]["beam_valid"]
-
-		beam2.id = data["transducers"][2]["id"]
-		beam2.velocity = data["transducers"][2]["velocity"]
-		beam2.distance = data["transducers"][2]["distance"]
-		beam2.rssi = data["transducers"][2]["rssi"]
-		beam2.nsd = data["transducers"][2]["nsd"]
-		beam2.valid = data["transducers"][2]["beam_valid"]
-
-		beam3.id = data["transducers"][3]["id"]
-		beam3.velocity = data["transducers"][3]["velocity"]
-		beam3.distance = data["transducers"][3]["distance"]
-		beam3.rssi = data["transducers"][3]["rssi"]
-		beam3.nsd = data["transducers"][3]["nsd"]
-		beam3.valid = data["transducers"][3]["beam_valid"]
-
-		theDVL.beams = [beam0, beam1, beam2, beam3]
-
-		pub.publish(theDVL)
+			rospy.logwarn("Unknown DVL message type: {}".format(msg_type))
 
 		rate.sleep()
 
